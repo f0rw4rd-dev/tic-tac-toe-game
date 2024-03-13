621157f3e9b09f2d10277ed511108b2b4d157dc3
f0rw4rd-dev <52598385+f0rw4rd-dev@users.noreply.github.com>
2024-03-13 18:48:42 +0300

    Fix bugs

--- main.py
+++ main.py
@@ -31,31 +31,53 @@ def check_winner(board):
 
 @app.post('/games/create')
 def create_game(username: str):
-    player = Player(username=username) if username not in players else players[username]
     game_id = str(uuid.uuid4())
+
+    if username in players:
+        player = players[username]
+
+        if player.status != 0:
+            raise HTTPException(status_code=400, detail='Игрок уже в игре')
+        else:
+            player.status = 1
+            player.game_id = game_id
+    else:
+        player = Player(username=username, game_id=game_id, status=1)
+        players[username] = player
+
     side = random.choice(['X', 'O'])
-    game = Game(id=game_id, players=[player], sides={side: player}, turn=player, player_status={username: 0})
-    players[player.username] = player
-    games[game_id] = game
+    games[game_id] = Game(id=game_id, sides={side: player}, turn=player, players={username: player})
 
     return {'game_id': game_id, 'side': side}
 
 
 @app.post('/games/join')
 def join_game(game_id: str, username: str):
-    player = Player(username=username) if username not in players else players[username]
     game = games.get(game_id)
 
     if not game:
         raise HTTPException(status_code=404, detail='Игра не найдена')
 
-    if len(game.players) >= 2:
+    if len(game.players) == 2:
         raise HTTPException(status_code=400, detail='К игре уже подключено 2 человека')
 
+    if username in players:
+        player = players[username]
+
+        if player.status != 0:
+            raise HTTPException(status_code=400, detail='Игрок уже в игре')
+        else:
+            player.status = 1
+            player.game_id = game_id
+    else:
+        player = Player(username=username, game_id=game_id, status=1)
+        players[username] = player
+
     side = 'X' if game.sides['X'] is None else 'O'
+
     game.sides[side] = player
-    game.players.append(player)
-    players[player.username] = player
+    game.players[username] = player
+    game.status = 1
 
     return {'side': side}
 
@@ -67,10 +89,10 @@ def make_move(game_id: str, username: str, x: int, y: int):
     if not game:
         raise HTTPException(status_code=404, detail='Игра не найдена')
 
-    if game.status > 0:
-        raise HTTPException(status_code=400, detail='Игра окончена')
+    if game.status == 0:
+        raise HTTPException(status_code=400, detail='Игра еще не началась')
 
-    player = next((p for p in game.players if p.username == username), None)
+    player = game.players.get(username)
 
     if not player:
         raise HTTPException(status_code=400, detail='Игрок не присоединен к данной игре')
@@ -84,28 +106,42 @@ def make_move(game_id: str, username: str, x: int, y: int):
     if game.turn != player:
         raise HTTPException(status_code=400, detail='Сейчас ход другого игрока')
 
+    k_restart = 0
+    for p_username, p_player in game.players.items():
+        if p_player.status == -1:
+            k_restart += 1
+
+    if k_restart > 0:
+        raise HTTPException(status_code=400, detail='Ещё не все игроки перезагружены')
+    elif k_restart == 0:
+        game.status = 1
+
+    if game.status == 2:
+        raise HTTPException(status_code=400, detail='Игра уже окончена')
+
     game.board[x][y] = 'X' if player == game.sides['X'] else 'O'
     game.turn = game.sides['O'] if player == game.sides['X'] else game.sides['X']
 
     winner = check_winner(game.board)
 
-    if winner:
+    if winner:  # Победа
         player.victories += 1
-        game.victories[game.board[x][y]] += 1
         game.status = 2
+        game.victories[game.board[x][y]] += 1
 
-        for p in game.players:
-            p.status = -1
-    elif all([cell != '' for row in game.board for cell in row]):
+        for p_username, p_player in game.players.items():
+            p_player.status = -1
+    elif all([cell != '' for row in game.board for cell in row]):  # Ничья
+        game.status = 2
         game.draws += 1
+
+        for p_username, p_player in game.players.items():
+            p_player.status = -1
+    else:
         game.status = 1
 
-        for p in game.players:
-            p.status = -1
-    else:
-        game.status = 0
-
-    game.last_move_time[username] = datetime.now()
+        for p_username, p_player in game.players.items():
+            p_player.status = 0
 
     return {'status': game.status}
 
@@ -117,6 +153,9 @@ def get_board(game_id: str, username: str):
     if not game:
         raise HTTPException(status_code=404, detail='Игра не найдена')
 
+    if username not in game.players:
+        raise HTTPException(status_code=400, detail='Игрок не присоединен к данной игре')
+
     board = {}
     for i in range(3):
         for j in range(3):
@@ -124,7 +163,7 @@ def get_board(game_id: str, username: str):
 
     board.update({
         'turn': 'X' if game.sides['X'] == game.turn else 'O',
-        'side': 'X' if game.sides['X'] == next((p for p in game.players if p.username == username), None) else 'O',
+        'side': 'X' if game.sides['X'] is not None and game.sides['X'].username == username else 'O',
         'victories.X': game.victories['X'],
         'victories.O': game.victories['O'],
         'draws': game.draws,
@@ -141,34 +180,39 @@ def restart_game(game_id: str, username: str):
     if not game:
         raise HTTPException(status_code=404, detail='Игра не найдена')
 
-    player = next((p for p in game.players if p.username == username), None)
+    player = game.players.get(username)
 
     if not player:
         raise HTTPException(status_code=400, detail='Игрок не присоединен к данной игре')
 
-    # if game.status == 0:
-    #     raise HTTPException(status_code=400, detail='Игра еще не окончена')
+    if game.status != 2:
+        raise HTTPException(status_code=400, detail='Игра еще не окончена')
 
-    for p in game.players:
-        if p.username == username:
-            if p.status == 0:
-                raise HTTPException(status_code=400, detail='Игра еще не окончена')
+    k_restart = 0
+    for p_username, p_player in game.players.items():
+        if p_player.status == -1:
+            k_restart += 1
+
+        if p_username == username:
+            if p_player.status != -1:
+                raise HTTPException(status_code=400, detail='Игрок уже перезагружен')
             else:
-                p.status = 0
+                p_player.status = 1
 
     game.board = [['', '', ''], ['', '', ''], ['', '', '']]
-    game.last_move_time = {'X': datetime.now(), 'O': datetime.now()}
-    game.sides = {'X': None, 'O': None}
-    game.turn = player
-    game.status = 0
 
-    side = random.choice(['X', 'O'])
-    opposite_side = 'O' if side == 'X' else 'X'
+    if k_restart == 2:
+        game.sides = {'X': None, 'O': None}
+        game.turn = player
 
-    opposite_player = next((p for p in game.players if p.username != username), None)
+        side = random.choice(['X', 'O'])
+        game.sides[side] = player
+    elif k_restart == 1:
+        side = 'X' if game.sides['X'] is None else 'O'
+    else:
+        raise HTTPException(status_code=400, detail='Игра не требует перезапуска')
 
     game.sides[side] = player
-    game.sides[opposite_side] = opposite_player
 
     return {'side': side}
 
